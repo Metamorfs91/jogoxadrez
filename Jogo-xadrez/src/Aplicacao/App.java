f8ee8b1cb950af5b54a889811d177978722d5b7e
Fernando <euevcfn@gmail.com>
2024-01-17 14:59:30 -0300

    Primeira Classe: Posicao

--- Jogo-xadrez/src/Aplicacao/App.java
+++ Jogo-xadrez/src/Aplicacao/App.java
@@ -1,7 +1,10 @@
 package Aplicacao;
 
+import Tabuleiro.Posicao;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Posicao = pos = new Posicao(3, 5);
     }
+
 }
